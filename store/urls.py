7456248ce4b2b4de85b4fd9a50f81dd71e21d177
kazafy <kazafysayed@gmail.com>
2017-04-25 14:25:26 +0200

    add routes && templetes

---
+++ store/urls.py
@@ -0,0 +1,10 @@
+from django.conf.urls import url
+from . import views
+urlpatterns = [
+
+    url(r'^book/$', views.getBooks),
+    url(r'^book/(?P<book_id>[0-9]+)$',views.getBook),
+
+    url(r'^author/$', views.getAuthors),
+    url(r'^author/(?P<author_id>[0-9]+)$', views.getAuthor),
+]
--- store/views.py
+++ store/views.py
@@ -1,3 +1,29 @@
 from django.shortcuts import render
-
+from .models import *
 # Create your views here.
+
+
+def index(request):
+    pass
+
+
+def getBook(request,book_id):
+    book= Book.objects.get(id=book_id)
+    return render(request,'book.html', {'book': book})
+
+
+def getBooks(request):
+    books = Book.objects.all()
+    print(len(books))
+    return render(request,'books.html', {'books': books})
+
+
+def getAuthor(request,author_id):
+    author= Author.objects.get(id=author_id)
+    return render(request,'author.html', {'author': author})
+
+
+def getAuthors(request):
+    authors = Author.objects.all()
+    return render(request,'authors.html', {'authors': authors})
+
