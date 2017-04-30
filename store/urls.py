04257234a236db85580dcc16d878fe5423957047
kazafy <kazafysayed@gmail.com>
2017-04-30 15:45:54 +0200

    read $ want to read

--- store/urls.py
+++ store/urls.py
@@ -2,11 +2,15 @@ from django.conf.urls import url
 from . import views
 urlpatterns = [
 
-    url(r'^book/$', views.getBooks),
-    url(r'^book/(?P<book_id>[0-9]+)$',views.getBook),
 
+    url(r'^book/search/(?P<key>\s+)$', views.getBookBy),
+    url(r'^book/$', views.getBooks),
+    url(r'^book/(?P<book_id>[0-9]+)$', views.getBook),
+    url(r'^book/(?P<book_id>[0-9]+)/(?P<state>[0-9]+)$',views.wantToRead),
     url(r'^author/$', views.getAuthors),
     url(r'^author/(?P<author_id>[0-9]+)$', views.getAuthor),
+    url(r'^author/follow/(?P<author_id>[0-9]+)$', views.followAuthor),
+    url(r'^author/unfollow/(?P<author_id>[0-9]+)$', views.unFollowAuthor),
     url(r'^register/$', views.register, name='register'),
 url(r'^login/$', views.login, name='login')
 ]
